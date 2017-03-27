bf2f59952a760ce71b07935b7f170cf8de7d10fc
nisargkumar.s.patil <nisargkumar.s.patil@accenture.com>
2017-03-27 18:22:11 +0530

    1. Deleting the unwanted class 2. Removing unwanted code from displayClass.java

--- src/com/test/displayClass.java
+++ src/com/test/displayClass.java
@@ -4,7 +4,6 @@ public class displayClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi there this is just a display class");
-		System.out.println("some other stuff");
 	}
 
 }
--- src/com/test/sfsfdfdf.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class sfsfdfdf {
-public void mi()
-{
-	System.out.println("This is mi() method");
-	}
-}

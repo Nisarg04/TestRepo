2976db3048a2dd05334c75ac0ff8a2782c6053d7
nisargkumar.s.patil <nisargkumar.s.patil@accenture.com>
2017-03-27 19:09:55 +0530

    disabled the git staging view.

--- src/com/test/displayClass.java
+++ src/com/test/displayClass.java
@@ -3,7 +3,6 @@ package com.test;
 public class displayClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hi there this is just a display class");
+		System.out.println("Hi there this is just a display class!");
 	}
-
 }

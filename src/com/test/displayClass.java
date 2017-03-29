53c6cf51f1cfd2ae420e65024572eeff0549bbe9
nisargkumar.s.patil <nisargkumar.s.patil@accenture.com>
2017-03-29 19:48:49 +0530

    added one more syso statement

--- src/com/test/displayClass.java
+++ src/com/test/displayClass.java
@@ -3,6 +3,7 @@ package com.test;
 public class displayClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hi there this is just a display class!");
+		System.out.println("Hi there this is just a display class!!!");
+		System.out.println("Added one more syso!!");
 	}
 }

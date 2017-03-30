3d4cf7ea50dc0378cadc66419e99b6315ff7c849
nisargkumar.s.patil <nisargkumar.s.patil@accenture.com>
2017-03-30 20:22:39 +0530

    conflict check

--- src/com/test/displayClass.java
+++ src/com/test/displayClass.java
@@ -4,6 +4,6 @@ public class displayClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi there this is just a display class!!!");
-		System.out.println("Added one more syso!!");
+		System.out.println("checking conflicts");
 	}
 }

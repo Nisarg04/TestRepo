0b48f40d09cfee63e2f6c701ce389d229bd802bf
Nisarg04 <patil_nisarg@yahoo.com>
2017-07-29 23:52:57 +0530

    adding comparable demo

---
+++ src/com/comparable/ComparableDemo.java
@@ -0,0 +1,22 @@
+package com.comparable;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class ComparableDemo{
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee(10, "Mikey", 25, 10000);
+		Employee e2 = new Employee(20, "Arun", 29, 20000);
+		Employee e3 = new Employee(5, "Lisa", 35, 5000);
+		Employee e4 = new Employee(1, "Pankaj", 32, 50000);
+		
+		List<Employee> empList = Arrays.asList(e1,e2,e3,e4);
+		System.out.println("Before Sorting : " + empList.toString());
+		Collections.sort(empList);
+		System.out.println("After Sorting : " + empList.toString());
+	}
+
+}

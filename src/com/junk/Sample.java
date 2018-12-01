bc4678b4bf922b8b5b1c48a74a1008ec84d3f0b8
nisarg <patil_nisarg@yahoo.com>
2018-12-01 13:16:38 +0530

    adding a file to feature branch

---
+++ src/com/junk/Sample.java
@@ -0,0 +1,25 @@
+package com.junk;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		String str= "sample program java program the occurences the java program";
+        String[] inputstr = str.split(" ");
+        
+        
+        Map<String, Long> countMap = Arrays.stream(inputstr)
+                .collect(Collectors.groupingBy(Object::toString, Collectors.counting()));
+        
+        Map<String, List<String>> collect = Arrays.stream(inputstr)
+        		.collect(Collectors.groupingBy(Object::toString));
+        
+        System.out.println(collect);
+        System.out.println(countMap);
+	}
+
+}

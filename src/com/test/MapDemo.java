2f2de711464dabe52373404bc323a9ac0f541bbe
nisarg <patil_nisarg@yahoo.com>
2018-12-01 13:49:43 +0530

    adding nisarg as a new user

--- src/com/test/MapDemo.java
+++ src/com/test/MapDemo.java
@@ -12,18 +12,22 @@ class MapDemo extends Thread {
 		SampleKey k3 = new SampleKey("Ramesh",3);
 		SampleKey k4 = new SampleKey("Suresh",4);
 		SampleKey k5 = new SampleKey("Wuresh",5);
-
+		SampleKey k6 = new SampleKey("Nisarg",9);
+		
 		map.put(k1,25);
 		map.put(k3,23);
 		map.put(k2,24);
 		map.put(k4,35);
 		map.put(k5,76);
+		map.put(k6,76);
 		
 		System.out.println(k1.key + " " + (k1.hashCode() & 15));
 		System.out.println(k2.key + " " + (k2.hashCode() & 15));
 		System.out.println(k3.key + " " + (k3.hashCode() & 15));
 		System.out.println(k4.key + " " + (k4.hashCode() & 15));
 		System.out.println(k5.key + " " + (k5.hashCode() & 15));
+		System.out.println(k6.key + " " + (k5.hashCode() & 15));
+		
 		System.out.println(map);
 		
 		

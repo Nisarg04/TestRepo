b89bce7ec3f352e12051653324348a1ae125533b
nisarg <patil_nisarg@yahoo.com>
2018-12-01 13:27:49 +0530

    adding memory package

---
+++ src/com/memory/valuesandreferences/Main.java
@@ -0,0 +1,36 @@
+package com.memory.valuesandreferences;
+
+
+/**
+ * Try to dry run hte program using stack and heap,
+ * and check it with actually running the program
+ * 
+ * 
+ * @author admin
+ *
+ * 
+ */
+public class Main {
+
+	public static void main(String args[]) {
+		Main main = new Main();
+		main.start();
+	}
+	
+	public void start() {
+		String last = "Z";
+		Container container = new Container();
+		container.setInitial("C");
+		another(container,last);
+		System.out.print(container.getInitial());
+	}
+	
+	public void another(Container initialHolder, String newInitial) {
+		newInitial.toLowerCase();
+		initialHolder.setInitial("B");
+		Container initial2 = new Container();
+		initialHolder=initial2;
+		System.out.print(initialHolder.getInitial());
+		System.out.print(newInitial);
+	}
+}
